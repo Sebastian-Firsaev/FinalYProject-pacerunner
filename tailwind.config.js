81f7c4e40ac698b2e8452e9081793834bbc7c2ba
Sebastian Firsaev <91489950+Sebastian-Firsaev@users.noreply.github.com>
2024-04-11 10:01:05 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This will include all files with .js, .jsx, .ts, and .tsx extensions in the src directory and its subdirectories
+    "./public/index.html", // This includes the main HTML file
+  ],
   theme: {
     extend: {},
   },
